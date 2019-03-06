ea72c1c71b36b3f712175d5b25ed1c8796c9b034
Hanif Imam <decadex99@gmail.com>
2019-03-06 23:37:50 +0700

    Update 02_PSD_PG_01_HanifImam_1706043273.vhd

--- Modul 2/02_PSD_PG_01_HanifImam_1706043273.vhd
+++ Modul 2/02_PSD_PG_01_HanifImam_1706043273.vhd
@@ -1,4 +1,8 @@
 library IEEE;
+--- Nama  : Hanif Imam
+--- NPM   : 1706043273
+--- Modul : Modul 02, Up Down Counter
+
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
@@ -9,19 +13,12 @@ entity UDCount is
 end entity;
 
 architecture menghitung of UDCount is
-	signal clk_div : STD_LOGIC_VECTOR (3 downto 0) := X"0";
 	signal itung   : STD_LOGIC_VECTOR (7 downto 0) := X"00";
 begin
-	process (clk)
-	begin
-		if (clk'Event and clk = '1') then
-			clk_div <= clk_div + '1';
-		end if;
-	end process;
 
-	process (clk_div(3), arah)
+	process (clk, arah)
 	begin
-		if (clk_div(3)'Event and clk_div(3) = '1') then
+		if rising_edge(clk) then
 			if (arah = '1') then
 				itung <= itung + '1';
 			elsif (arah = '0') then
