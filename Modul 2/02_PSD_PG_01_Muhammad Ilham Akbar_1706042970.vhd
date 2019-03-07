48bc1f5c83b2443da0c487c3e1f9a80250f26b28
muhilham01 <39519269+muhilham01@users.noreply.github.com>
2019-03-07 08:25:59 +0700

    Add files via upload

---
+++ Modul 2/02_PSD_PG_01_Muhammad Ilham Akbar_1706042970.vhd
@@ -0,0 +1,33 @@
+--COUNTER UPDOWN
+-- Muhammad Ilham Akbar/1706042970
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity Counter_Up_Down1 is
+    Port ( clock		: in std_logic; -- input Clock
+		   up_down		: in std_logic; -- up atau down
+           reset		: in std_logic; -- input Reset 
+           counter_out	: out std_logic_vector(3 downto 0) -- output 4-bit
+     );
+end entity;
+
+architecture Behavioral of Counter_Up_Down1 is
+signal count_updown: std_logic_vector(3 downto 0)
+:="0000";
+
+begin
+process(clock,reset) 
+begin
+if(rising_edge(clock)) then
+	if(reset='1') then
+        count_updown <= x"0";
+		elsif(up_down='0') then
+			count_updown <= count_updown - x"1"; -- counter down
+		else 
+			count_updown <= count_updown + x"1"; -- counter up
+    end if;
+ end if;
+end process;
+counter_out <= count_updown;
+end architecture;
