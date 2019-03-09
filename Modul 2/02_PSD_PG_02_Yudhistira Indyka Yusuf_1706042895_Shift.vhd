22fbb16c71637240a9d9d97864f1f0526c34bd1b
Yudhistira Indyka <33085123+yudhistiraindyka@users.noreply.github.com>
2019-03-09 13:05:30 +0700

    Add files via upload

---
+++ Modul 2/02_PSD_PG_02_Yudhistira Indyka Yusuf_1706042895_Shift.vhd
@@ -0,0 +1,42 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity shift_top is
+    Port ( CLK : in  STD_LOGIC;
+           D   : in  STD_LOGIC;
+           LED : out STD_LOGIC_VECTOR(7 downto 0));
+end shift_top;
+    
+architecture Behavioral of shift_top is
+    signal clock_div : STD_LOGIC_VECTOR(4 downto 0);
+    signal shift_reg : STD_LOGIC_VECTOR(7 downto 0) := X"00";
+begin
+
+    -- clock divider
+    process (CLK)
+    begin
+        if (CLK'event and CLK = '1') then
+            clock_div <= clock_div + '1';
+        end if;
+    end process;
+    
+    -- shift register
+    process (clock_div(4))
+    begin
+        if (clock_div(4)'event and clock_div(4) = '1') then
+            shift_reg(7) <= D;
+            shift_reg(6) <= shift_reg(7);
+            shift_reg(5) <= shift_reg(6);
+            shift_reg(4) <= shift_reg(5);
+            shift_reg(3) <= shift_reg(4);
+            shift_reg(2) <= shift_reg(3);
+            shift_reg(1) <= shift_reg(2);
+            shift_reg(0) <= shift_reg(1);
+        end if;
+    end process;
+    
+    -- hook up the shift register bits to the LEDs
+    LED <= shift_reg;
+
+end Behavioral;
