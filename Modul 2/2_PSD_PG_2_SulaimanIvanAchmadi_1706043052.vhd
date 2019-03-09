d7ddf3c7aa006fe1188fa37843dd1bd9e07df0c1
ivanachmadi <39551945+ivanachmadi@users.noreply.github.com>
2019-03-09 14:34:46 +0700

    Add files via upload

---
+++ Modul 2/2_PSD_PG_2_SulaimanIvanAchmadi_1706043052.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.std_logic_unsigned.ALL;
+
+entity up_down_counter is
+    Port ( CLK : in  std_logic;
+           DIR : in  std_logic;
+           LED : out  std_logic_vector (7 downto 0));
+end up_down_counter;
+
+architecture behavioral of up_down_counter2 is
+    signal clk_div : std_logic_vector (5 downto 0);
+    signal count   : std_logic_vector (7 downto 0);
+begin
+    process (CLK)
+    begin
+        if (CLK'Event and CLK = '1') then
+            clk_div <= clk_div + '1';
+        end if;
