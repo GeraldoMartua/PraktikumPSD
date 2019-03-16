4e94ab0b9e4ce952eb90cceb3efc8a8a4a455c5a
MarinusMartin <48108988+MarinusMartin@users.noreply.github.com>
2019-03-16 14:28:21 +0700

    Update 03_PSD_PG_02_Marinus Martin Dwiantoro_1706042775.vhd

--- Modul 3/03_PSD_PG_02_Marinus Martin Dwiantoro_1706042775.vhd
+++ Modul 3/03_PSD_PG_02_Marinus Martin Dwiantoro_1706042775.vhd
@@ -5,7 +5,7 @@ USE IEEE.NUMERIC_STD.ALL;
 ENTITY SANDIRUMPUT IS
 PORT( 
 NAME : IN STRING (10 DOWNTO 1) := "HAI FAKHRUL!";
-TEMP : OUT STD_LOGIC := '0');
+SRUMPUT : OUT STD_LOGIC := '0');
 END SANDIRUMPUT;
 
 ARCHITECTURE Behaviour OF SANDIRUMPUT IS
@@ -16,105 +16,105 @@ BEGIN
 		FOR x in 10 DOWNTO 1 LOOP
 
 	IF NAME(x)='H' THEN --H
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
 	ELSIF NAME(x)='A' THEN --A
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
 	ELSIF NAME(x)='I' THEN --I
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
    	ELSIF NAME(x)=' ' THEN --Space
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 2 SEC;
    
 	ELSIF NAME(X)='F' THEN --F
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
    	ELSIF NAME(x)='A' THEN --A
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 			
 	ELSIF NAME(x)='K' THEN --K
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
 	ELSIF NAME(x)='H' THEN --H
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
 	ELSIF NAME(x)='R' THEN --R
@@ -133,41 +133,41 @@ BEGIN
 
    
 	ELSIF NAME(x)='U' THEN --U
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
 
     	ELSIF NAME(x)='L' THEN --L
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1.5 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP <= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.2 SEC;
-		TEMP<= '1';
+		SRUMPUT <= '1';
 			WAIT FOR 1 SEC;
-		TEMP <= '0';
+		SRUMPUT <= '0';
 			WAIT FOR 0.5 SEC;
   
 	END IF;
 END LOOP;
 
-		TEMP <= '0';
-			WAIT FOR 10 SEC;
+		SRUMPUT <= '0';
+			WAIT FOR 100 SEC;
 	END PROCESS;
 END Behaviour;
